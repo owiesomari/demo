c7b62dd9fe424980f3c2ef0792541045fefab71e
Oweis Alomari <oweis.alomari@arabbank.com.jo>
2022-09-21 21:24:52 +0300

    add validators

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -12,9 +12,22 @@ export class UserProfileComponent implements OnInit {
   private lname: HTMLInputElement | undefined
   private email: HTMLInputElement | undefined
   private phone: HTMLInputElement | undefined
+  private country: HTMLInputElement | undefined
+  private city: HTMLInputElement | undefined
+  private place: HTMLInputElement | undefined
+  private address1: HTMLInputElement | undefined
+  private address2: HTMLInputElement | undefined
+  private postal: HTMLInputElement | undefined
+  private company: HTMLInputElement | undefined
+  private shopify: HTMLInputElement | undefined
+  private facebook: HTMLInputElement | undefined
+  private insta: HTMLInputElement | undefined
+  private oldPassword: HTMLInputElement | undefined
+  private newPassword: HTMLInputElement | undefined
+  private confirmNewPassword: HTMLInputElement | undefined
+  private marketerImage: HTMLImageElement | undefined
 
-  private validator:Validator | undefined
-
+  private validator: Validator | undefined
 
   constructor() {
     this.validator = new Validator()
@@ -33,14 +46,14 @@ export class UserProfileComponent implements OnInit {
     info1.style.display = "none"
     info2.style.display = "none"
 
-    if(this.validator?.isElementEmpty(this.email)){
-      info1.innerText= "هذا الحقل مطلوب";
+    if (this.validator?.isElementEmpty(this.email)) {
+      info1.innerText = "هذا الحقل مطلوب";
       info1.style.display = "block";
       return false;
     }
 
-    if(this.validator?.isElementEmpty(this.phone)){
-      info2.innerText= "هذا الحقل مطلوب";
+    if (this.validator?.isElementEmpty(this.phone)) {
+      info2.innerText = "هذا الحقل مطلوب";
       info2.style.display = "block";
       return false;
     }
@@ -51,10 +64,22 @@ export class UserProfileComponent implements OnInit {
   ngOnInit(): void {
     this.fname = document.getElementById("fname") as HTMLInputElement
     this.lname = document.getElementById("lname") as HTMLInputElement
+    this.marketerImage = document.getElementById("marketerImage") as HTMLImageElement
     this.email = document.getElementById("email") as HTMLInputElement
     this.phone = document.getElementById("phone") as HTMLInputElement
-
-
+    this.country = document.getElementById("country") as HTMLInputElement
+    this.city = document.getElementById("city") as HTMLInputElement
+    this.place = document.getElementById("place") as HTMLInputElement
+    this.address1 = document.getElementById("address1") as HTMLInputElement
+    this.address2 = document.getElementById("address2") as HTMLInputElement
+    this.postal = document.getElementById("postal") as HTMLInputElement
+    this.company = document.getElementById("company") as HTMLInputElement
+    this.shopify = document.getElementById("shopify") as HTMLInputElement
+    this.facebook = document.getElementById("facebook") as HTMLInputElement
+    this.insta = document.getElementById("insta") as HTMLInputElement
+    this.oldPassword = document.getElementById("oldPassword") as HTMLInputElement
+    this.newPassword = document.getElementById("newPassword") as HTMLInputElement
+    this.confirmNewPassword = document.getElementById("confirmNewPassword") as HTMLInputElement
 
     this.fname.value = "Owies"
     this.lname.value = "Alomari"
