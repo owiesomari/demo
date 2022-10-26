f2a754e921fee3548ba7586c83cc2157c2f755e5
Oweis Alomari <oweis.alomari@arabbank.com.jo>
2022-10-26 21:44:08 +0300

    marketer- orders

--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Order } from 'src/app/Entities/Order';
+import { Modal } from 'bootstrap';
 
 @Component({
   selector: 'app-orders',
@@ -10,13 +11,19 @@ export class OrdersComponent implements OnInit {
 
   orders: Order[] = [];
   tempOrders: Order[] = [];
+
+  modalElement: HTMLElement | undefined
+  modalComponent: Modal | undefined
+
+  modalOrderID = ""
+
   constructor() {
     this.orders = [
-      new Order(1, "12/10/2022", "owies", 12,"https://www.google.com/", "عند الاستلام" , 12, "قيد التسليم","#FFB959"),
-      new Order(2, "12/10/2022", "ahmed", 12,"https://www.google.com/",  "عند الاستلام", 12, "مكتملة","#67CFA2"),
-      new Order(3, "12/10/2022", "abdullah", 12,"https://www.google.com/",  "عند الاستلام", 12, "ملغية","#CE0000"),
-      new Order(5, "12/10/2022", "noor", 12,"https://www.google.com/",  "عند الاستلام", 12, "معلقة","#FC7383"),
-      new Order(6, "12/10/2022", "ali", 12,"https://www.google.com/","عند الاستلام", 12, "معلقة","#FC7383"),
+      new Order(1, "12/10/2022", "owies", 12, "", "عند الاستلام", 12, "قيد التسليم", "#FFB959"),
+      new Order(2, "12/10/2022", "ahmed", 12, "https://www.google.com/", "عند الاستلام", 12, "مكتملة", "#67CFA2"),
+      new Order(3, "12/10/2022", "abdullah", 12, "https://www.google.com/", "عند الاستلام", 12, "ملغية", "#CE0000"),
+      new Order(5, "12/10/2022", "noor", 12, "https://www.google.com/", "عند الاستلام", 12, "معلقة", "#FC7383"),
+      new Order(6, "12/10/2022", "ali", 12, "https://www.google.com/", "عند الاستلام", 12, "معلقة", "#FC7383"),
     ];
     this.tempOrders = this.orders;
   }
@@ -24,7 +31,7 @@ export class OrdersComponent implements OnInit {
   filterTable(event: any) {
     let buttonsParent = (document.getElementById("buttons") as HTMLDivElement).childNodes
     for (var i = 0; i < buttonsParent.length; i++) {
-        (buttonsParent[i].childNodes[0] as HTMLButtonElement).style.background = "#fff"
+      (buttonsParent[i].childNodes[0] as HTMLButtonElement).style.background = "#fff"
     }
 
     (document.getElementById("all") as HTMLButtonElement).style.background = "#ce0000"
@@ -37,7 +44,7 @@ export class OrdersComponent implements OnInit {
 
 
   filterCategoty(event: any) {
-    let id = event.target.id;
+   let id  = event.target.id;
     this.setFilterButtonBackground(id);
     switch (id) {
       case "all": {
@@ -72,9 +79,35 @@ export class OrdersComponent implements OnInit {
   private setFilterButtonBackground(id: string) {
     let buttonsParent = (document.getElementById("buttons") as HTMLDivElement).childNodes
     for (var i = 0; i < buttonsParent.length; i++) {
-        (buttonsParent[i].childNodes[0] as HTMLButtonElement).style.background = "#fff"
+      (buttonsParent[i].childNodes[0] as HTMLButtonElement).style.background = "#fff"
     }
-      (document.getElementById(id) as HTMLButtonElement).style.background = "#ce0000"
+    (document.getElementById(id) as HTMLButtonElement).style.background = "#ce0000"
+  }
+
+  openModal(event: any) {
+    this.modalOrderID = event?.target.id
+    this.fillModal(this.modalOrderID)
+    this.modalElement = document.getElementById('modal') as HTMLElement;
+    this.modalComponent = new Modal(this.modalElement);
+    this.modalComponent.show();
+  }
+
+  private fillModal(id: string) {
+    var a = this.orders.filter((obj) => {
+      return obj.getID().toString() == id;
+    })[0];
+    (document.getElementById("c_orderNumber") as HTMLSpanElement).innerText = a.getID().toString();
+  }
+
+  closeModal() {
+    this.modalComponent?.hide();
+
+  }
+
+  cancelOrder() {
+    console.log(this.modalOrderID);
+    //call api with modalOrderID
+
   }
 
   ngOnInit(): void {
